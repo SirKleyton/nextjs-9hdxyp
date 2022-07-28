a6e95dc4f58a915b19d7f3ecc58cbf29a9610e54
SirKleyton <cleyton_ferr@hotmail.com>
2022-07-28 13:34:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-9hdxyp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-9hdxyp)
---
+++ cleiton.js
@@ -0,0 +1,6 @@
+const meuConjunto = new Set();
+
+const times = ['São Paulo', 'Flamengo', 'Fluminense', 'Palmeiras', 'São Paulo'];
+const meuCampeonato = new Set(times);
+
+console.table(meuCampeonato);
